0c80aa9f74d7c49d15fcd28e91a97009ff9a293f
Sergio De Luca <sdlbsso@gmail.com>
2022-08-06 15:10:13 -0300

    crud con ruby on rails

---
+++ db/migrate/20220805204742_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.string :string
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
